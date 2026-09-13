9b48c6ae1c9f3aff0f2741ff9f77953b0996e2ac
EmadAden77 <sasfsas8@gmail.com>
2026-09-13 21:47:40 +0300

    Delete priority rules

--- rules/priority-rules.js
+++
@@ -1,6 +0,0 @@
-(function(g){
-  const M=g.MasterRules=g.MasterRules||{};
-  M.levels=Object.freeze({CRITICAL:100,PHYSICAL:80,SCENE:60,AESTHETIC:40});
-  M.rules=M.rules||[];
-  M.register=rule=>M.rules.push(Object.freeze({...rule}));
-})(window);
